789d658ad38b719c1bde85ba660ca4b3eee25e21
Matheus Cavalcante Teixeira <matheus.mteixeira@mercadolivre.com>
2021-12-16 19:12:33 -0300

    update build folder in script saucelabs

--- ExampleSwift/scripts/saucelabs.sh
+++ ExampleSwift/scripts/saucelabs.sh
@@ -1,4 +1,4 @@
 curl -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
 --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
---form 'payload=@"build/ExampleSwift.ipa"' \
+--form 'payload=@"../build/ExampleSwift.ipa"' \
 --form 'name="ExampleSwift.ipa"'
