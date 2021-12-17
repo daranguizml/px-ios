106b9038bfc567c118b19489d2f1f77c1d8815b4
Matheus Cavalcante Teixeira <matheus.mteixeira@mercadolivre.com>
2021-12-17 15:15:43 -0300

    update saucelabs file to update fileid

--- ExampleSwift/scripts/saucelabs.sh
+++ ExampleSwift/scripts/saucelabs.sh
@@ -2,4 +2,4 @@ fileId=curl -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
 --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
 --form 'payload=@"../build/ExampleSwift.ipa"' \
 --form 'name="ExampleSwift.ipa"'
-envman add --key SAUCELABS_FILE_ID_UPLOADED --value $fileId
+envman add --key SAUCELABS_FILE_ID_UPLOADED -value $fileId
