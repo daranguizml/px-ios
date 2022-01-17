efd2ee3191895a33f069466b94ff32e6a00d6f64
Jona <jonathan.scaramal@mercadolibre.com>
2022-01-17 17:31:39 -0300

    chore: Added mocked response to checkout request

--- MercadoPagoSDK/MercadoPagoSDK/RequestLayer/Checkout/CheckoutRequestInfos.swift
+++ MercadoPagoSDK/MercadoPagoSDK/RequestLayer/Checkout/CheckoutRequestInfos.swift
@@ -46,6 +46,15 @@ extension CheckoutRequestInfos: RequestInfos {
     }
 
     var mockURL: URL? {
-        return nil
+        //     return URL(string: "https://run.mocky.io/v3/3e0e367c-15e8-4228-8480-440ed6df41ad") // con dto
+        //     return URL(string: "https://run.mocky.io/v3/1c40eea7-6a92-460f-b657-2f44b1235a37")
+            // return URL(string: "https://run.mocky.io/v3/7c74a637-f400-4a70-952d-bd9c40828913")// letras blancas
+            // return URL(string: "https://run.mocky.io/v3/0f81c669-caaa-4e02-b94a-db3c04e4287e") // BBVA
+//            return URL(string: "https://run.mocky.io/v3/36c6bbea-5a10-49b8-b081-711e13cc0723")// BBVA con msg resaltado
+//            return URL(string: "https://run.mocky.io/v3/0ce399b8-1075-48fd-a2fa-ef90cfee580b") // con payerPM
+        // return URL(string: "https://run.mocky.io/v3/e5705c9e-7d61-4211-a056-d978c18d461c")!
+        return URL(string: "https://run.mocky.io/v3/4bd07c00-57dc-4423-b4a9-538ca925c7ea")!
+
+//        return nil
     }
 }
