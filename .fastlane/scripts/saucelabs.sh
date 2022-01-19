d697aa8facd12f4319254c1ccf5de1cc7cff4593
Matheus Cavalcante Teixeira <matheus.mteixeira@mercadolivre.com>
2022-01-19 19:15:26 -0300

    update upload to saucelabs script var names

--- .fastlane/scripts/saucelabs.sh
+++ .fastlane/scripts/saucelabs.sh
@@ -1,4 +1,4 @@
-fileId=$(curl -sb -H -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" --location \
+fileId=$(curl -sb -H -u "$SAUCELABS_USERNAME:$SAUCELABS_ACCESS_KEY" --location \
 --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
 --form 'payload=@"../build/ExampleSwift.ipa"' \
 --form 'name="ExampleSwift.ipa"' | json item.id) 
